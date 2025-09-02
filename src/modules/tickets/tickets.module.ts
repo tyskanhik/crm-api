58940b403476ecef675cc0a7b9831e0bf9ab56e9
Arctic Fox <tyskanhik@mail.ru>
2025-09-02 21:33:30 +0300

    create tickets module and update tickets controller

---
+++ src/modules/tickets/tickets.module.ts
@@ -0,0 +1,20 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Ticket } from "./entities/ticket.entity";
+import { TicketsController } from "./tickets.controller";
+import { TicketsService } from "./services/tickets.service";
+import { TicketRepository } from "./repositories/ticket.repository";
+
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Ticket])],
+  controllers: [TicketsController],
+  providers: [
+    TicketsService,
+    {
+      provide: 'ITicketRepository',
+      useClass: TicketRepository
+    }
+  ]
+})
+export class TicketsModule {}
