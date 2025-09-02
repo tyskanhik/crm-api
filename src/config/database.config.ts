d127ec24a849cea65e6804481102478c8b975725
Arctic Fox <tyskanhik@mail.ru>
2025-09-02 16:01:36 +0300

    add database config and module

---
+++ src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('database', () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'crm_api',
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: process.env.NODE_ENV !== 'production',
+}))
