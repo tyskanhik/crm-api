7ec362aba9c347bc7eec00efcdc5c76cf09a557e
Arctic Fox <tyskanhik@mail.ru>
2025-09-02 21:34:52 +0300

    edit app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { DatabaseModule } from './shared/database/database.module';
+import { TicketsModule } from './modules/tickets/tickets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig]
+    }),
+    DatabaseModule,
+    TicketsModule,
+  ],
 })
 export class AppModule {}
