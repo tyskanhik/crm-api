329cb12d14daee03ed1cb5952b174ba2bf5e8cda
Arctic Fox <tyskanhik@mail.ru>
2025-09-08 14:06:09 +0300

    create entite

---
+++ src/modules/attachments/entities/attachment.entity.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Ticket } from '../../tickets/entities/ticket.entity';
+
+@Entity('attachments')
+export class Attachment {
+  @ApiProperty()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty()
+  @Column()
+  objectName: string;
+
+  @ApiProperty()
+  @Column()
+  originalName: string;
+
+  @ApiProperty()
+  @Column()
+  mimetype: string;
+
+  @ApiProperty()
+  @Column('int')
+  size: number;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ApiProperty()
+  @Column('uuid')
+  ticketId: string;
+
+  @ManyToOne(() => Ticket, ticket => ticket.attachments, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'ticketId' })
+  ticket: Ticket;
+
+  constructor(partial: Partial<Attachment>) {
+    Object.assign(this, partial);
+  }
+}
