a326a151fa6080d27831a856650f88590971a491
Arctic Fox <tyskanhik@mail.ru>
2025-09-08 13:08:44 +0300

    create min_io config

---
+++ src/config/minio.config.ts
@@ -0,0 +1,10 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('minio', () => ({
+  endPoint: process.env.MINIO_ENDPOINT || 'localhost',
+  port: parseInt(process.env.MINIO_PORT ?? '9000', 10),
+  useSSL: process.env.MINIO_USE_SSL === 'true',
+  accessKey: process.env.MINIO_ACCESS_KEY || 'minioadmin',
+  secretKey: process.env.MINIO_SECRET_KEY || 'minioadmin',
+  bucketName: process.env.MINIO_BUCKET || 'attachments',
+}));
